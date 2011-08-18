4af1796709064124822b474783dda6b5e56cb443
unknown <eleneromagico@hotmail.com>
2011-08-18 11:10:16 +0200

    Completado catalogo general

--- menuG.php
+++ menuG.php
@@ -1,6 +1,6 @@
 <?php
-session_start();
-if(!isset ($_SESSION['usuario'])) die("Debe logearse primero");
+include "funciones.php";
+compruebaSesion();
 ?>        
 <!DOCTYPE html>
 <html>
@@ -11,22 +11,42 @@ if(!isset ($_SESSION['usuario'])) die("Debe logearse primero");
     <body>
         <h1> MENU</h1>
         <?php
-        
         if (!$_SESSION['usuario'] || !isset($_SESSION['usuario'])) {
             echo "logeate primero";
         } else {
             $usuario = $_SESSION['usuario'];
             echo "Usuario:" . $usuario['nombre_usuario'];
-            
-            
-            
         }
         ?>
         <h1>Opciones</h1>   
-        <ul>
-            <li><a href='consulta_general.php'>Consulta general</a>
-            <li><a href='consulta_concreta.php'>Consulta concreta</a>    
+        <ul><li>Consultas
+                <ul>
+                    <li><a href='consulta_general.php'>Consulta general</a>
+                    <li><a href='consulta_concreta.php'>Consulta concreta</a>    
+
+                </ul>
+                <?php
+                // Comprobacion del tipo de usuario
+                if ($usuario['tipo_usuario_id_tipo_usuario'] == 0)
+                    mostrarOpcionesAdministracion();
+                ?>
+
             <li><a href='salida.php'>Salir</a>
         </ul>
+
     </body>
 </html>
+
+<?php
+
+function mostrarOpcionesAdministracion() {
+//echo "Es administrador";
+    echo("<li>Administracion
+                <ul>
+                    <li><a href=''>Cat&aacute;logo</a>
+                    <li><a href=''>Usuarios</a>    
+                        
+
+                </ul>");
+}
+?>
